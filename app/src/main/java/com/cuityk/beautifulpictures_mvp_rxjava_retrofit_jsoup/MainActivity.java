2ef990b6b23a3a00523fdcf387bee2e0bef23ba4
yinkai <1362616851@qq.com>
2018-04-19 14:03:51 +0800

    初始化项目

---
+++ app/src/main/java/com/cuityk/beautifulpictures_mvp_rxjava_retrofit_jsoup/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cuityk.beautifulpictures_mvp_rxjava_retrofit_jsoup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
